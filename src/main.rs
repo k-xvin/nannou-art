c60c517cd5c86d6ae18e6cbc51b11e79683e28d4
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-04-27 02:08:02 -0700

    nannou example

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
 use nannou::prelude::*;
 
 fn main() {
-    nannou::app(model)
-        .update(update)
-        .simple_window(view)
-        .run();
+    nannou::app(model).update(update).simple_window(view).run();
 }
 
 struct Model {}
 
-fn model(_app: &App) -> Model {
+fn model(app: &App) -> Model {
+    //println!("I was here {}", app.window_count());
+    app.main_window().set_title("ayop");
     Model {}
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
-}
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn view(_app: &App, _model: &Model, frame: Frame){
-    frame.clear(PURPLE);
+fn view(app: &App, _model: &Model, frame: Frame) {
+    // Prepare to draw.
+    let draw = app.draw();
+
+    // Generate sine wave data based on the time of the app
+    let sine = app.time.sin();
+    let slowersine = (app.time / 2.0).sin();
+
+    // Get boundary of the window (to constrain the movements of our circle)
+    let boundary = app.window_rect();
+
+    // Map the sine wave functions to ranges between the boundaries of the window
+    let x = map_range(sine, -1.0, 1.0, boundary.left(), boundary.right());
+    let y = map_range(slowersine, -1.0, 1.0, boundary.bottom(), boundary.top());
+
+    // draws background once
+    if app.time <= 0.01 {
+        draw.background().color(PLUM);
+    }
+    
+
+    // Draw a blue ellipse at the x/y coordinates 0.0, 0.0
+    draw.ellipse().color(STEELBLUE).x_y(x, y);
+
+    draw.to_frame(app, &frame).unwrap();
 }
