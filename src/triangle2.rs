9a9109468b678ddc5a78322d508e8161bf2ce63f
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-05-21 23:24:54 -0700

    now there are triangles
    but i think the code is kind of badly written

--- src/triangle2.rs
+++ src/triangle2.rs
@@ -13,19 +13,21 @@ fn model(app: &App) -> Model {
 
     let mut points = [pt2(0.0, 0.0); 100];
     for i in 1..100 {
-        let x = random_range(-400, 401) as f32;
-        let y = random_range(-400, 401) as f32;
+        let x = random_range(-350, 351) as f32;
+        let y = random_range(-350, 351) as f32;
         points[i] = pt2(x, y);
     }
     Model { points }
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    //let mut points = [pt2(0.0, 0.0); 100];
-    for i in 1..100 {
-        let x = random_range(-400, 401) as f32;
-        let y = random_range(-400, 401) as f32;
-        model.points[i] = pt2(x, y);
+    // only update every 60 frames
+    if _app.elapsed_frames() % 60 == 0 {
+        for i in 1..100 {
+            let x = random_range(-350, 351) as f32;
+            let y = random_range(-350, 351) as f32;
+            model.points[i] = pt2(x, y);
+        }
     }
 }
 
@@ -33,20 +35,32 @@ fn view(app: &App, model: &Model, frame: Frame) {
     // Prepare to draw.
     let draw = app.draw();
 
-    let color = rgba(
-        STEELBLUE.into_components().0 as f32 / 255.0,
-        STEELBLUE.into_components().1 as f32 / 255.0,
-        STEELBLUE.into_components().2 as f32 / 255.0,
-        0.3,
-    );
-    for i in 0..model.points.len() {
-        draw.ellipse()
-            .color(color)
-            .w(3.0)
-            .h(3.0)
-            .x_y(model.points[i].x, model.points[i].y);
+    // let color = rgba(
+    //     STEELBLUE.into_components().0 as f32 / 255.0,
+    //     STEELBLUE.into_components().1 as f32 / 255.0,
+    //     STEELBLUE.into_components().2 as f32 / 255.0,
+    //     0.3,
+    // );
+    let colors = [STEELBLUE, ALICEBLUE, REBECCAPURPLE, DARKSEAGREEN];
+    for i in 0..(model.points.len() - 2) {
+        // draw.ellipse()
+        //     .color(STEELBLUE)
+        //     .w(3.0)
+        //     .h(3.0)
+        //     .x_y(model.points[i].x, model.points[i].y);
+
+        draw.tri()
+            //.color(colors[i%4])
+            .color(rgba(
+                colors[i % 4].into_components().0 as f32 / 255.0,
+                colors[i % 4].into_components().1 as f32 / 255.0,
+                colors[i % 4].into_components().2 as f32 / 255.0,
+                i as f32 / 255.0,
+            ))
+            .points(model.points[i], model.points[i + 1], model.points[i + 2]);
     }
-    //draw.background().color(BLACK);
+    //draw.background().color(WHEAT);
+    draw.background().color(BLACK);
 
     draw.to_frame(app, &frame).unwrap();
 }
